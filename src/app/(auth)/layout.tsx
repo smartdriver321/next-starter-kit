2a6af751e9fece036b9e1db0203b8f5e02389181
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-10-02 10:41:16 +0700

    Create HomeLayout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,13 +1,17 @@
 import React, { ReactNode } from 'react'
 
 import SiteHeader from '@/components/frontend/site-header'
+import Footer from '@/components/frontend/site-footer'
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
 	const session = null
 	return (
 		<div className='bg-white'>
 			<SiteHeader session={session} />
-			<div className='relative isolate px-6 pt-14 lg:px-8'>{children}</div>
+			<div className='relative isolate px-6 pt-14 lg:px-8'>
+				{children}
+				<Footer />
+			</div>
 		</div>
 	)
 }
